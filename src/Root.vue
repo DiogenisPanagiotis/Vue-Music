fee19188c9349e8bd00038fd64bdac8cb541c615
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-01-31 02:28:14 -0500

    Add dynamic album playlist widget.

--- src/Root.vue
+++ src/Root.vue
@@ -1,18 +1,23 @@
 <template>
     <div class='container'>
       <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
           <br>
             <div class='filter'>
-              <input type='text' v-on:keyup.enter='search()' class='form-control fontAwesome' id="query" placeholder="&#xf002;"></input>
+              <input type='text' v-on:keyup.enter='searchAlbums()' class='form-control fontAwesome' id="query" placeholder="&#xf002;"></input>
             </div>
+            <br>
+            <iframe :src="iframeSource" width="555" height="300" frameborder="0" allowtransparency="true"></iframe>
         </div>
       </div>
       <br>
       <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-xs-11 col-xs-offset-1  col-sm-6 col-sm-offset-3">
             <div class='albums' v-for="album in albums">
               <img @click='playAlbum' class='rounded' height='127' width='127' :src='album[0]' :albumId='album[2]' :alt='album[1]'>  
+            </div>
+            <div class='albums' v-for="track in tracks">
+              <img @click='playAlbum' class='rounded' height='127' width='127' :src='track[0]' :albumId='track[2]' :alt='track[1]'>  
             </div>
         </div>
       </div>
@@ -27,24 +32,42 @@
     data(){
       return {
         albums: [],
+        tracks: [],
+        iframeSource: ''
       }
     }, 
-    mounted(){
-      this.getArticles();
-    },
     methods: {
-      search: function(query){
+      searchAlbums: function(query){
         this.albums = [];
         query = document.getElementById('query').value;
         let that = this;
 
         this.$http.get('https://api.spotify.com/v1/search', {params:  {query: query, type: 'album'}})
           .then(res => {
+            console.log(res.body);
             for (var i = 0; i < res.body.albums.items.length; i++){
               let image = res.body.albums.items[i].images[0].url;
               let title = res.body.albums.items[i].name;
               let albumId = res.body.albums.items[i].id;
               that.albums.push([image, title, albumId]);
+            }
+            let iframeSource = 'https://embed.spotify.com/?uri=spotify:album:' + that.albums[0][2] + '&theme=white';
+            this.iframeSource = iframeSource;
+            this.searchTracks(query);
+        });
+      },
+      searchTracks: function(query){
+        this.tracks = [];
+        query = document.getElementById('query').value;
+        let that = this;
+
+        this.$http.get('https://api.spotify.com/v1/search', {params:  {query: query, type: 'track'}})
+          .then(res => {
+            for (var i = 0; i < res.body.tracks.items.length; i++){
+              let image = res.body.tracks.items[i].album.images[0].url;
+              let title = res.body.tracks.items[i].name;
+              let albumId = res.body.tracks.items[i].album.id;
+              that.tracks.push([image, title, albumId]);
             }
         });
       },
@@ -57,6 +80,8 @@
       playAlbum: function(e){
         let targ = e.target;
         let playing = 'playing';
+        let iframeSource = 'https://embed.spotify.com/?uri=spotify:album:' + targ.getAttribute('albumId') + '&theme=white';
+        this.iframeSource = iframeSource;
 
          if (targ !== null) {
           if (targ.classList.contains(playing)){
@@ -87,7 +112,7 @@
   .filter {
     padding: 40px;
     background: #efefef;
-    border-radius: 5px;
+    border-radius: 3px;
   }
   h5 {
     display: inline;
@@ -106,6 +131,7 @@
   .playing {
     border: 5px solid #fff;
     border-radius: 8px;
+    opacity: 0.5;
   }
   input {
     border-radius: 1px;
@@ -116,5 +142,12 @@
   }
   .fontAwesome {
     font-family: 'Helvetica', FontAwesome, sans-serif;
+  }
+  iframe {
+    /*border: 10px solid #fff;*/
+    /*border: 5px;
+    border-radius: 5px;
+    border-color: #efefef;*/
+    /*margin-left: 125px;*/
   }
 </style>
